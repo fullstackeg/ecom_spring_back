9b5622bae17ad8129183205c70b529b149913644
Anouar <anouar.laachir2024@gmail.com>
2025-08-25 14:32:20 +0100

    adding Client Entity

---
+++ src/main/java/com/fullstack/ecom_spring_back/entity/Client.java
@@ -0,0 +1,24 @@
+package com.fullstack.ecom_spring_back.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "clients")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fullName;
+    private String email;
+    private String phone;
+    @OneToMany(mappedBy = "client", cascade = CascadeType.ALL)
+    private List<Sell> sells;
+}
