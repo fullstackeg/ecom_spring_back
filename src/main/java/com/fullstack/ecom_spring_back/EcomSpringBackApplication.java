af3f111978e688ee195d543c95cf4fefe7d3392f
Anouar <anouar.laachir2024@gmail.com>
2025-08-25 23:38:44 +0100

    Enable JPA Auditing

--- src/main/java/com/fullstack/ecom_spring_back/EcomSpringBackApplication.java
+++ src/main/java/com/fullstack/ecom_spring_back/EcomSpringBackApplication.java
@@ -2,8 +2,10 @@ package com.fullstack.ecom_spring_back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class EcomSpringBackApplication {
 
     public static void main(String[] args) {
