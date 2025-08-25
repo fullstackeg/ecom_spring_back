94e0251b6c056b785ba25020b1fd8dd97bf6a70b
Anouar <anouar.laachir2024@gmail.com>
2025-08-25 14:23:45 +0100

    starter project

---
+++ src/main/java/com/fullstack/ecom_spring_back/EcomSpringBackApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack.ecom_spring_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomSpringBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomSpringBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fullstack/ecom_spring_back/EcomSpringBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.ecom_spring_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomSpringBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
