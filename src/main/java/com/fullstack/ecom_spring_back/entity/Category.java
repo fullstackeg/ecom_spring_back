809408cdd3ce3851f31dac69e03bc716fef230a2
Anouar <anouar.laachir2024@gmail.com>
2025-08-26 17:17:22 +0100

    Some Updates to fix some problems

--- src/main/java/com/fullstack/ecom_spring_back/entity/Category.java
+++ src/main/java/com/fullstack/ecom_spring_back/entity/Category.java
@@ -1,10 +1,7 @@
 package com.fullstack.ecom_spring_back.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.List;
 
@@ -14,7 +11,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Category {
+public class Category extends  BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- src/main/java/com/fullstack/ecom_spring_back/entity/Product.java
+++ src/main/java/com/fullstack/ecom_spring_back/entity/Product.java
@@ -1,10 +1,7 @@
 package com.fullstack.ecom_spring_back.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Table(name = "products")
