47d65c5d8d820e872ff74704c924d59a0a2dad78
Anouar <anouar.laachir2024@gmail.com>
2025-08-26 20:01:20 +0100

    fix a banal error in confirmSell and cancelSell @PathVariable instead of @RequestBody (copy/paste thing haha)

--- src/main/java/com/fullstack/ecom_spring_back/controller/SellController.java
+++ src/main/java/com/fullstack/ecom_spring_back/controller/SellController.java
@@ -5,10 +5,7 @@ import com.fullstack.ecom_spring_back.repository.SellRepository;
 import com.fullstack.ecom_spring_back.service.SellService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/sells")
@@ -26,14 +23,14 @@ public class SellController {
 
     //POST: Confirm Sell
     @PostMapping("/confirm/{id}")
-    public ResponseEntity<Sell> confirmSell(@RequestBody Long id) {
+    public ResponseEntity<Sell> confirmSell(@PathVariable Long id) {
         Sell saved = sellService.confirmSell(id);
         return ResponseEntity.ok(saved);
     }
 
     //POST: Confirm Sell
     @PostMapping("/cancel/{id}")
-    public ResponseEntity<Sell> cancelSell(@RequestBody Long id) {
+    public ResponseEntity<Sell> cancelSell(@PathVariable Long id) {
         Sell saved = sellService.cancelSell(id);
         return ResponseEntity.ok(saved);
     }
