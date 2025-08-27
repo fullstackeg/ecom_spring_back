b0714e0a9587b9681a73814f1d9721790ea37b20
Anouar <anouar.laachir2024@gmail.com>
2025-08-28 00:01:33 +0100

    Adding SecurityConfig class to manage roles and authentication

---
+++ src/main/java/com/fullstack/ecom_spring_back/utils/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.fullstack.ecom_spring_back.utils;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/auth/**").permitAll() //login / register are public
+                    .requestMatchers("/api/products/**").hasRole("ADMIN") //only ADMIN
+                    .requestMatchers("/api/sells/**").hasAnyRole("ADMIN", "USER") //both can access
+                    .anyRequest().authenticated()
+                 )
+            .httpBasic(Customizer.withDefaults()); //basic auth (switch to JWT Later)
+        return http.build();
+    }
+}
