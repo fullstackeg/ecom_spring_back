24931ffe3137c4a5a4ae10db4ce6a176ef723298
Anouar <anouar.laachir2024@gmail.com>
2025-08-27 20:57:12 +0100

    update GlobalExceptionHandler class adding MethodArgumentTypeMismatchException handling

--- src/main/java/com/fullstack/ecom_spring_back/utils/GlobalExceptionHandler.java
+++ src/main/java/com/fullstack/ecom_spring_back/utils/GlobalExceptionHandler.java
@@ -3,6 +3,12 @@ package com.fullstack.ecom_spring_back.utils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -10,5 +16,24 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
         // return the exception message as response body
         return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentTypeMismatchException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        if(ex.getRequiredType() != null && ex.getRequiredType().isEnum()) {
+            Class<?> enumType = ex.getRequiredType();
+            String allowedValues = Arrays.stream(enumType.getEnumConstants())
+                    .map(Object::toString)
+                    .collect(Collectors.joining(", "));
+            errors.put("error", "Invalid value for " + ex.getName());
+            errors.put("message", "Allowed values are: " + allowedValues);
+        } else {
+            errors.put("error", "Invalid Parameter");
+            errors.put("message", ex.getMessage());
+        }
+
+        return ResponseEntity.badRequest().body(errors);
     }
 }
