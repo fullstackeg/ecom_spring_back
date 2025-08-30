a80b460c7f8beec42052593941a414257c656b6e
Anouar <anouar.laachir2024@gmail.com>
2025-08-30 23:52:30 +0100

    Updating securityFilterChain method and adding authenticationManager method to SecurityConfig

--- src/main/java/com/fullstack/ecom_spring_back/utils/SecurityConfig.java
+++ src/main/java/com/fullstack/ecom_spring_back/utils/SecurityConfig.java
@@ -1,18 +1,27 @@
 package com.fullstack.ecom_spring_back.utils;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthFilter jwtAuthFilter;
+    private final CustomAuthEntryPoint customAuthEntryPoint;
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, CustomAuthEntryPoint customAuthEntryPoint) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, CustomAuthEntryPoint customAuthEntryPoint)
+            throws Exception {
         http
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> auth
@@ -21,14 +30,21 @@ public class SecurityConfig {
                     .requestMatchers("/api/sells/**").hasAnyRole("ADMIN", "USER") //both can access
                     .anyRequest().authenticated()
                  )
-            .httpBasic(Customizer.withDefaults()) //basic auth (switch to JWT Later)
-                .exceptionHandling(ex -> ex
-                        .authenticationEntryPoint(customAuthEntryPoint)); //handle 401
+            .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(ex -> ex.authenticationEntryPoint(customAuthEntryPoint))
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        
         return http.build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
+    throws Exception {
+        return config.getAuthenticationManager();
     }
 }
